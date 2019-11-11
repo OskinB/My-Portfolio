0aacd14fc2a0c650cec2c6352ecf5fbc173a865d
Ósk Björns <oskb.h17@nemandi.ntv.is>
2019-11-11 01:09:54 +0000

    css/sass adjusted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+      <nav>
+        <h1>Ósk Björns</h1>
+        <h2>Jr. Web Developer</h2>
+      </nav>
+      <hr />
+
     </div>
   );
 }
